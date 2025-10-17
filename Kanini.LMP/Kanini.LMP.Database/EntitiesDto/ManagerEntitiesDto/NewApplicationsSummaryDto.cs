12e33526e7367a84205795db30f47a8e8e396355
Charan <gcharan6366@gmail.com>
2025-10-17 17:33:26 +0530

    Updated DTO for manager dashboard

---
+++ Kanini.LMP/Kanini.LMP.Database/EntitiesDto/ManagerEntitiesDto/NewApplicationsSummaryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanini.LMP.Database.EntitiesDto.ManagerEntitiesDto
+{
+    internal class NewApplicationsSummaryDto
+    {
+        public int TotalNewApplications { get; set; }
+        public int ApprovedCount { get; set; } // Count of recently approved
+        public int PendingCount { get; set; }  // Count of recently submitted/pending 
+        public decimal ApprovedPercentage { get; set; } 
+
+    }
+}
