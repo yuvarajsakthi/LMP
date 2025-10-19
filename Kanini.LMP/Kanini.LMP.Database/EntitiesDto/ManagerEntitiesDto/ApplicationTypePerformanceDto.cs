e14fd7cad80e83319f3239ff6db9bcbcb8dee575
yuvaraj <yuvarajsakthi003@gmail.com>
2025-10-19 22:17:43 +0530

    added viewstatus

--- Kanini.LMP/Kanini.LMP.Database/EntitiesDto/ManagerEntitiesDto/ApplicationTypePerformanceDto.cs
+++ Kanini.LMP/Kanini.LMP.Database/EntitiesDto/ManagerEntitiesDto/ApplicationTypePerformanceDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Kanini.LMP.Database.EntitiesDto.ManagerEntitiesDto
+﻿namespace Kanini.LMP.Database.EntitiesDto.ManagerEntitiesDto
 {
     internal class ApplicationTypePerformanceDto
     {
