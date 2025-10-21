7ac46dd925f6c460d3d2146ffe12d058b377ef20
yuvaraj <yuvarajsakthi003@gmail.com>
2025-10-21 17:42:49 +0530

    changed the names

--- Kanini.LMP/Kanini.LMP.Database/Enums/LoanPaymentStatus.cs
+++ Kanini.LMP/Kanini.LMP.Database/Enums/LoanPaymentStatus.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Kanini.LMP.Database.Enums
+﻿namespace Kanini.LMP.Database.Enums
 {
     public enum LoanPaymentStatus
     {
