17bc440504383622ba05bbb75decebfc8bb2db92
pujannagari-pallavi <pujannagaripallavi@gmail.com>
2025-12-04 12:03:56 +0530

    Updated

---
+++ Kanini.LMP/Kanini.LMP.Application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+using Kanini.LMP.Database.EntitiesDto;
+using Kanini.LMP.Database.EntitiesDtos.Authentication;
+
+namespace Kanini.LMP.Application.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDTO?> GetUserByNameAsync(string username);
+        Task<UserDTO?> GetUserByIdAsync(int userId);
+        Task<UserDTO?> GetUserByEmailAsync(string email);
+        Task<UserDTO> RegisterCustomerAsync(CustomerRegistrationDTO registrationDto);
+        Task<bool> ForgotPasswordAsync(string email);
+        Task<UserDTO> CreateUserAsync(UserDTO userDto);
+    }
+}
